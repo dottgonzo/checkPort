add76f77bcf0318f8b889b8953fa6faeee98374c
dario <dev@dariocaruso.info>
2018-05-14 10:53:27 +0200

    copied

---
+++ index.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Promise = require("bluebird");
+const async = require("async");
+const net = require("net");
+function checkPort(port) {
+    return new Promise((resolve, reject) => {
+        let available = false;
+        const s = net.createServer().listen(port, '0.0.0.0');
+        s.on('listening', () => {
+            available = true;
+            s.close();
+        });
+        s.on('error', () => {
+            available = false;
+            s.close();
+        });
+        s.on('close', () => {
+            if (available)
+                reject('invalid port');
+            resolve({ port: port });
+        });
+    });
+}
+function checkFreePortFromPool(portPool) {
+    return new Promise((resolve, reject) => {
+        async.eachSeries(portPool, (p, cb) => {
+            checkPort(p).then((port) => {
+                if (port)
+                    return resolve(port);
+            }).catch((err) => {
+                cb();
+            });
+        }, (err) => {
+            if (err)
+                return reject(err);
+            reject('not found');
+        });
+    });
+}
---
+++ index.ts
@@ -0,0 +1,51 @@
+import * as Promise from "bluebird"
+
+import * as async from "async"
+
+
+import * as net from "net"
+
+
+
+function checkPort(port: number) {
+  return new Promise<{ port: number }>((resolve, reject) => {
+    let available = false;
+    const s = net.createServer().listen(port, '0.0.0.0');
+    s.on('listening', () => {
+      available = true;
+      s.close();
+    });
+    s.on('error', () => {
+      available = false;
+      s.close();
+    });
+    s.on('close', () => {
+      if (available) reject('invalid port');
+      resolve({ port: port });
+    });
+  })
+}
+
+
+function checkFreePortFromPool(portPool: number[]) {
+  return new Promise<{ port: number }>((resolve, reject) => {
+
+    async.eachSeries(portPool, (p, cb) => {
+
+      checkPort(p).then((port) => {
+        if (port) return resolve(port)
+      }).catch((err) => {
+        cb()
+      })
+
+
+    }, (err) => {
+      if (err) return reject(err)
+      reject('not found')
+    })
+
+
+
+  })
+
+}
